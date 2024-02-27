e61a4c5dada9b7aa7d8c63acbc0045b5b6691eba
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-02-27 10:46:11 +0100

    feat: added filename check on diagram save

--- src/lib/diagrams/Topbar.svelte
+++ src/lib/diagrams/Topbar.svelte
@@ -5,21 +5,43 @@
 	import { tables } from '$stores/table';
 	import { addToast } from '$stores/toast';
 	import { IconChevronLeft } from '@tabler/icons-svelte';
-	import { BaseDirectory, renameFile, writeTextFile } from '@tauri-apps/api/fs';
+	import { BaseDirectory, exists, renameFile, writeTextFile } from '@tauri-apps/api/fs';
 
-	export let initial: IndexedTableModel[];
+	export const initial: IndexedTableModel[] = [];
 	export let filename: string;
 
-	$: name = filename.split(".").at(0)!;
-	$: extension = filename.split(".").at(1)!;
+	let alreadyUsed: boolean = false;
+
+	$: name = filename.split('.').at(0)!;
+	$: extension = filename.split('.').at(1)!;
 
 	$: newFilename = name;
 
 	const back = async () => {
-		await goto("/")
+		await goto('/');
+	};
+
+	const runFilenameChangeCheck = async () => {
+		alreadyUsed = await filenameAlreadyUsed();
 	}
 
+	const filenameAlreadyUsed = async (): Promise<boolean> =>
+		filename !== `${newFilename}.${extension}` &&
+		(await exists(`${MODEL_PATH}/${newFilename}.${extension}`, {
+			dir: BaseDirectory.Document
+		}));
+
 	const save = async () => {
+		if (await filenameAlreadyUsed()) {
+			addToast({ type: 'error', message: 'Filename already used by another diagram' });
+			return;
+		}
+
+		if (newFilename === "") {
+			addToast({ type: 'error', message: 'Filename cannot be empty' });
+            return;
+		}
+
 		await renameFile(
 			`${MODEL_PATH}/${name}.${extension}`,
 			`${MODEL_PATH}/${newFilename}.${extension}`,
@@ -39,13 +61,21 @@
 		<IconChevronLeft />
 		<span>My Diagrams</span>
 	</button>
-	<div class="flex items-center">
-		<input
-			bind:value={newFilename}
-			name="filename"
-			placeholder="Diagram file name"
-			class="w-auto bg-transparent"
-		/>
+	<div class="relative flex items-center">
+		<div class="relative flex justify-center">
+			<input
+				bind:value={newFilename}
+				name="filename"
+				placeholder="Diagram file name"
+				class="w-auto bg-transparent"
+				on:input={runFilenameChangeCheck}
+			/>
+			{#if alreadyUsed}
+				<p class="absolute text-center -bottom-[calc(100%+1rem)] mx-auto px-1 rounded-lg bg-red-600 text-white text-sm">
+					Filename already used
+				</p>
+			{/if}
+		</div>
 		<span>.{extension}</span>
 	</div>
 	<button on:click={save}>Save</button>
