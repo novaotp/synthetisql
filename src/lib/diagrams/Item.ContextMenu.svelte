b4fb78727945a0ed3575b2101ea7162c5f27e685
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-02-27 13:44:37 +0100

    feat: auto-width context menu

--- src/lib/diagrams/Item.ContextMenu.svelte
+++ src/lib/diagrams/Item.ContextMenu.svelte
@@ -6,8 +6,8 @@
     export let onClick: () => void;
 </script>
 
-<li class="block w-[1fr]">
-    <button class="text-[1rem] text-[#222] w-full h-8 text-start border-none bg-white" on:click={onClick}>
+<li class="relative block w-auto">
+    <button class="text-[1rem] text-[#222] w-full h-8 px-2 rounded-md flex gap-5 justify-between items-center text-start border-none bg-white hover:bg-gray-300" on:click={onClick}>
         <svelte:component this={icon} />
         <span>{label}</span>
     </button>
